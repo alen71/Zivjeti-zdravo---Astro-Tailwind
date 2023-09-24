7970117a4079aaf10c4e5e80078132dfdb87ef5e
alen71 <alen_babanovic123@hotmail.com>
2023-09-24 15:46:41 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
+import nodejs from '@astrojs/node'
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
