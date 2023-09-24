c4058a5bd02a9bc524fafc7757410edf53dc84a6
alen71 <alen_babanovic123@hotmail.com>
2023-09-24 15:45:14 +0200

    test

--- .netlify/functions-internal/chunks/_slug__304c1017.mjs
+++
@@ -1,7 +0,0 @@
-export { renderers } from '../renderers.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./prerender_6b4672b3.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/chunks/image-endpoint_439ed6b3.mjs
+++
@@ -1,7 +0,0 @@
-export { renderers } from '../renderers.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/image-endpoint_f5040fd7.mjs').then(n => n.i);
-
-export { page };
