a59cdbdf2decb585c589b4e0c6b9904c2c7cd5cc
alen71 <alen_babanovic123@hotmail.com>
2023-09-24 14:59:40 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import nodejs from '@astrojs/node'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   compressHTML: false,
   output: 'hybrid',
-  adapter: nodejs({
-    mode: 'middleware'
-  }),
+  adapter: netlify(),
+  // adapter: nodejs({
+  //   mode: 'middleware'
+  // }),
   image: {
     domains: ['wp.zivjetizdravo.com'],
     remotePatterns: [
