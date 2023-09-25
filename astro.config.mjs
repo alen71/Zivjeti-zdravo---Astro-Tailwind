f1121a733b9e1d98251dc9613d643e8ece6faf1a
alen71 <alen_babanovic123@hotmail.com>
2023-09-25 08:03:51 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify/functions'
 import nodejs from '@astrojs/node'
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   compressHTML: false,
-  output: 'server',
+  output: 'hybrid',
   adapter: netlify({
     edgeMiddleware: true,
-    functionPerRoute: true,
+    functionPerRoute: true
   }),
   // adapter: nodejs({
   //   mode: 'middleware'
   // }),
-  // image: {
-  //   domains: ['wp.zivjetizdravo.com'],
-  //   remotePatterns: [{
-  //     protocol: 'https'
-  //   }]
-  // }
-});
+  image: {
+    domains: ['wp.zivjetizdravo.com'],
+    remotePatterns: [
+      {
+        protocol: 'https'
+      }
+    ]
+  }
+})
